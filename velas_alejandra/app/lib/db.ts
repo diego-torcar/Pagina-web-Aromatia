64977c901d00be50b2087d74c71b910d2dddb845
diego-torcar <diego.al.to.ca@gmail.com>
2025-08-27 16:51:05 -0600

    ignorando prima

--- velas_alejandra/app/lib/db.ts
+++ velas_alejandra/app/lib/db.ts
@@ -4,9 +4,9 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["error", "warn"], // evita ruido en prod
-  });
+  (process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : new PrismaClient({ log: ["query", "error", "warn"] }));
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
